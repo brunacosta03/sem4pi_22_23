99b20fd189b8f9d25bae6159910872fc41402889
1201205samueldias <1201205@isep.ipp.pt>
2023-04-11 21:40:18 +0100

    fix: checkstyle

--- ecourse.bootstrappers/src/main/java/org/bootstrappers/ECourseBootstrapper.java
+++ ecourse.bootstrappers/src/main/java/org/bootstrappers/ECourseBootstrapper.java
@@ -16,24 +16,51 @@ import org.user.management.UserBuilderHelper;
 import persistence.PersistenceContext;
 
 public class ECourseBootstrapper implements Action {
+    /**
+     * The repository for managing user entities.
+     */
     private final UserRepository userRepository = PersistenceContext.repositories().users();
+    /**
+     * The authorization service for managing user authorization.
+     */
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    /**
+     * The authentication service for managing user authentication.
+     */
     private final AuthenticationService authenticationService = AuthzRegistry.authenticationService();
+    /**
+     * The username for the manager user used during bootstrapping.
+     */
     private static final String MANAGERBOOTSTRAP_USERNAME = "managerbootstrap";
+    /**
+     * The password for the manager user used during bootstrapping.
+     */
     private static final String MANAGERBOOTSTRAP_PASSWORD = "managerpassword";
 
+    /**
+     * Executes the bootstrapping action by registering the manager user.
+     * Invoke the users bootstrapper.
+     * @return true if the bootstrapping action is successful, false otherwise.
+     */
     @Override
     public boolean execute() {
         registerManagerUser(userRepository);
         authenticateForBootstrapping();
 
         UsersBootstrapper usersBootstrapper = new UsersBootstrapper();
-        System.out.println("Bootstrapper --> " + usersBootstrapper.getClass().getSimpleName());
+        System.out.println("Bootstrapper --> "
+                            + usersBootstrapper.getClass().getSimpleName());
 
         return usersBootstrapper.execute();
     }
 
-    public static boolean registerManagerUser(final UserRepository userRepository) {
+    /**
+     * Registers the manager user in the system during bootstrapping.
+     * @param userRepository the repository for managing user entities.
+     * @return true if the registration is successful, false otherwise.
+     */
+    public static boolean registerManagerUser(
+                            final UserRepository userRepository) {
         final SystemUserBuilder userBuilder = UserBuilderHelper.builder();
 
         userBuilder
@@ -53,8 +80,12 @@ public class ECourseBootstrapper implements Action {
         }
     }
 
+    /**
+     * Authenticates the manager user.
+     */
     protected void authenticateForBootstrapping() {
-        authenticationService.authenticate(MANAGERBOOTSTRAP_USERNAME, MANAGERBOOTSTRAP_PASSWORD);
+        authenticationService.authenticate(MANAGERBOOTSTRAP_USERNAME,
+                MANAGERBOOTSTRAP_PASSWORD);
         Invariants.ensure(authz.hasSession());
     }
 }
--- ecourse.persistence/src/main/java/org/persistence/JpaRepositoryFactory.java
+++ ecourse.persistence/src/main/java/org/persistence/JpaRepositoryFactory.java
@@ -7,16 +7,29 @@ import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 import org.ecourse.Application;
 import persistence.RepositoryFactory;
 
+/**
+ * Factory class for creating JPA repositories.
+ */
 public class JpaRepositoryFactory implements RepositoryFactory {
+    /**
+     * Creates a new transactional context for JPA repositories.
+     * @return transactional context.
+     */
     @Override
     public TransactionalContext newTransactionalContext() {
-        return JpaAutoTxRepository.buildTransactionalContext(Application.settings().persistenceUnitName(),
+        return JpaAutoTxRepository.buildTransactionalContext(
+                Application.settings().persistenceUnitName(),
                 Application.settings().extendedPersistenceProperties());
     }
 
+    /**
+     * Creates a new UserRepository implementation for JPA.
+     * @return UserRepository.
+     */
     @Override
     public UserRepository users() {
-        return new JpaAutoTxUserRepository(Application.settings().persistenceUnitName(),
+        return new JpaAutoTxUserRepository(
+                Application.settings().persistenceUnitName(),
                 Application.settings().extendedPersistenceProperties());
     }
 }
