38a3763b7777b9a93330e467eb2e8de7bc2b8d8b
1201205samueldias <1201205@isep.ipp.pt>
2023-04-11 20:01:21 +0100

    fix: problem with maven load

--- ecourse.app.user/src/main/java/org/user/Main.java
+++ ecourse.app.user/src/main/java/org/user/Main.java
@@ -4,21 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Modulo User a correr!");
 
-        try{
-            GrupoAutomovel grupoAutomovel = new GrupoAutomovel("teste", 4, "C");
-            GrupoAutomovelRepositorioJPAImpl ga = new GrupoAutomovelRepositorioJPAImpl();
-            ga.add(grupoAutomovel);
-        } catch (Exception e){
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println(e.getMessage());
-            System.out.println();
-            System.out.println();
-            System.out.println();
-        }
-
-        while(true){
+        while(true) {
+            //nothing happen atm
         }
     }
 }
