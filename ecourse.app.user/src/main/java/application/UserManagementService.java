93f11b3a2de218288cd3062167ecd202b72e08e2
henriquelspinto <1211201@isep.ipp.pt>
2023-04-12 23:54:40 +0100

    feat: Implementation of Auth and Authz, JPA In entities and documentation refactoring when needed #2 #5

--- ecourse.app.user/src/main/java/application/UserManagementService.java
+++ ecourse.app.user/src/main/java/application/UserManagementService.java
@@ -1,5 +1,6 @@
 package application;
 
+import domain.model.User;
 import domain.repositories.UserRepository;
 import eapli.framework.general.domain.model.EmailAddress;
 import eapli.framework.infrastructure.authz.application.PasswordPolicy;
@@ -7,6 +8,8 @@ import eapli.framework.infrastructure.authz.domain.model.*;
 import eapli.framework.time.util.CurrentTimeCalendars;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Optional;
 import java.util.Set;
@@ -22,43 +25,47 @@ public class UserManagementService {
         this.policy = policy;
         this.encoder = encoder;
     }
-
-    public SystemUser registerNewUser(final String username, final String rawPassword, final String firstName, final String lastName, final String email, final Set<Role> roles, final Calendar createdOn) {
+/*
+    public User registerNewUser(final String username, final String rawPassword, final String firstName, final String lastName, final String email, final Set<Role> roles, final Calendar createdOn) {
         SystemUserBuilder userBuilder = new SystemUserBuilder(this.policy, this.encoder);
         userBuilder.with(username, rawPassword, firstName, lastName, email).createdOn(createdOn).withRoles(roles);
-        SystemUser newUser = userBuilder.build();
-        return (SystemUser)this.userRepository.save(newUser);
+        User newUser = userBuilder.build();
+        return (User)this.userRepository.save(newUser);
     }
 
-    public SystemUser registerNewUser(final String username, final String rawPassword, final String firstName, final String lastName, final String email, final Set<Role> roles) {
+
+    public User registerNewUser(final String username, final String rawPassword, final String firstName, final String lastName, final String email, final Set<Role> roles) {
         return this.registerNewUser(username, rawPassword, firstName, lastName, email, roles, CurrentTimeCalendars.now());
     }
 
-    public SystemUser registerUser(final Username username, final Password password, final Name name, final EmailAddress email, final Set<Role> roles) {
+    public User registerUser(final Username username, final Password password, final Name name, final EmailAddress email, final Set<Role> roles) {
         SystemUserBuilder userBuilder = new SystemUserBuilder(this.policy, this.encoder);
         userBuilder.with(username, password, name, email).withRoles(roles);
-        SystemUser newUser = userBuilder.build();
-        return (SystemUser)this.userRepository.save(newUser);
+        User newUser = userBuilder.build();
+        return (User)this.userRepository.save(newUser);
     }
 
-    public Iterable<SystemUser> activeUsers() {
+
+ */
+
+    public Iterable<User> activeUsers() {
         return this.userRepository.findByActive(true);
     }
 
-    public Iterable<SystemUser> deactivatedUsers() {
+    public Iterable<User> deactivatedUsers() {
         return this.userRepository.findByActive(false);
     }
 
-    public Iterable<SystemUser> allUsers() {
+    public Iterable<User> allUsers() {
         return this.userRepository.findAll();
     }
 
-    public Optional<SystemUser> userOfIdentity(final Username id) {
+    public Optional<User> userOfIdentity(final Username id) {
         return this.userRepository.ofIdentity(id);
     }
 
-    public SystemUser deactivateUser(final SystemUser user) {
-        user.deactivate(CurrentTimeCalendars.now());
-        return (SystemUser)this.userRepository.save(user);
+    public User deactivateUser(final User user) {
+        user.deactivate();
+        return (User)this.userRepository.save(user);
     }
 }
