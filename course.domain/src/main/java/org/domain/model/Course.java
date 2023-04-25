0913d5f2c82fef69ac22bce106217443a02d2826
henriquelspinto <1211201@isep.ipp.pt>
2023-04-25 20:42:39 +0100

    feat: Course toString()

--- course.domain/src/main/java/org/domain/model/Course.java
+++ course.domain/src/main/java/org/domain/model/Course.java
@@ -169,4 +169,16 @@ public class Course implements AggregateRoot<CourseCode> {
                 "This teacher is not enrolled in this course");
         teachers.remove(teacher);
     }
+
+    @Override
+    public String toString() {
+        return "Course " + code + "\n" +
+                "Name: " + name + "\n" +
+                "Edition: " + edition + "\n" +
+                "Description: " + description + "\n" +
+                "State: " + state + "\n" +
+                "Max students: " + max + "\n" +
+                "Minimun students: " + min + "\n" +
+                "Head Teacher: " + headTeacher.emailAddress() + "\n";
+    }
 }
