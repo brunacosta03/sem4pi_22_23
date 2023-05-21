8034e3fabc10363fa9bf613ac7692b89571e66ad
brucosta0 <1211136@isep.ipp.pt>
2023-05-21 11:51:25 +0100

    US2001: Tests updated #13

--- exam.domain/src/main/java/org/domain/model/examtemplate/domain/ExamDescription.java
+++ exam.domain/src/main/java/org/domain/model/examtemplate/domain/ExamDescription.java
@@ -10,19 +10,33 @@ import javax.persistence.Embeddable;
 public class ExamDescription {
 
     @Column(name = "exam_description", nullable = false)
-    private String value;
+    private final String value;
 
-    private ExamDescription(String value){
-        Preconditions.ensure(value.length() >= 10, "Exam description must be at least 10 characters");
+    private ExamDescription(final String value){
+
+        Preconditions.nonNull(value);
+        Preconditions.ensure(!value.isEmpty(), "Description cannot be empty.");
 
         this.value = value;
     }
 
     protected ExamDescription() {
         // for ORM
+        value = "";
     }
 
-    public static ExamDescription of(String value){
+    public static ExamDescription of(final String value){
         return new ExamDescription(value);
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExamDescription)) return false;
+        ExamDescription that = (ExamDescription) o;
+        return value.equals(that.value);
     }
 }
