65908a76fbbf8cbde456e2a505492423ea5616c9
Henrique <1211201@isep.ipp.pt>
2023-06-13 01:04:20 +0100

    feat: HTTPS compatibility on our server (need to uncomment the serversocket creator, if wanted to use) #36

--- shared.board.server/src/main/java/org/shared/board/server/TcpSrv.java
+++ shared.board.server/src/main/java/org/shared/board/server/TcpSrv.java
@@ -6,9 +6,16 @@ import org.authz.application.AuthzRegistry;
 import org.persistence.PersistenceContext;
 import org.usermanagement.domain.model.ECoursePasswordPolicy;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import javax.net.ssl.*;
 
 /**
  * The type Tcp srv.
@@ -52,6 +59,11 @@ class TcpSrv {
         try {
             tcpSock = new ServerSocket(TCP_PORT);
             httpSock = new ServerSocket(HTTP_PORT);
+            /*httpSock = initializeSSLServerSocket(HTTP_PORT,
+                    "shared.board.server/src/main/resources/keystore/keystore.pfx",
+                    "ecourse",
+                    "ecourse");*/
+
             System.out.println("Server is listening");
         } catch (IOException ex) {
             System.out.println("Failed to open server socket");
@@ -84,5 +96,35 @@ class TcpSrv {
 
             new Thread(new TcpSrvThread(tpcCliSock)).start();
         }
+    }
+
+    private static ServerSocket initializeSSLServerSocket(int port, String keystorePath, String keystorePassword, String keyPassword)
+            throws IOException, NoSuchAlgorithmException, KeyManagementException,
+            UnrecoverableKeyException, KeyStoreException, CertificateException, IllegalArgumentException {
+        // Load the keystore
+        KeyStore keystore = KeyStore.getInstance("JKS");
+
+        File file = new File(keystorePath);
+
+        if(!file.exists())
+            System.out.println("Keystore file not found");
+
+        try (InputStream keystoreInputStream = new FileInputStream(keystorePath)) {
+            keystore.load(keystoreInputStream, keystorePassword.toCharArray());
+        }
+
+        // Create the key manager factory
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keystore, keyPassword.toCharArray());
+
+        // Create the SSL context
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+        // Create the SSL server socket factory
+        SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
+
+        // Create and return the SSL server socket
+        return sslServerSocketFactory.createServerSocket(port);
     }
 }
